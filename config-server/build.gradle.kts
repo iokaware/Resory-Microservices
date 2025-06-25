e948a6b579441c2ca30f37d64e91d31323aeee42
Ibrahim O Kaware <88645485+iokaware@users.noreply.github.com>
2025-06-25 12:57:33 +0300

    Add files via upload

---
+++ config-server/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.3"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.resory"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2025.0.0"
+
+dependencies {
+	implementation("org.springframework.cloud:spring-cloud-config-server")
+	implementation("org.springframework.cloud:spring-cloud-starter-config")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+	platform("org.springframework.cloud:spring-cloud-dependencies:2025.0.0")
+
+
+	// Lombok, if you are using it in this project
+	// compileOnly("org.projectlombok:lombok")
+	// kapt("org.projectlombok:lombok")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
